5b6de7a8a7c087cb396e0195742b3ce5e2e8113b
dvj-shimbo <dvj.shimbo@gmail.com>
2018-08-21 01:02:11 +0200

    about section added

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,103 +5,34 @@ $(window).on("load", function() {
 
 //	console.log("scripts.js is loaded");
 
-/**
-    $(".work-sample a").mouseover(function() {
-
-
-
-        var workSample = $(this);
-
-
-        if (workSample.attr("data-mouseover") !== "true") {
-
-            console.log("over");
-
-            workSample.attr("data-mouseover", "true");
-
-//            workSample.animate({
-//                "margin-left": "5%"
-//            }, 400).animate({
-//                "margin-left": ""
-//            }, 400);
-
-
-
-			if (! workSample.find(".icon-arrow-right").length) {
-
-                workSample.append("<div class=\"icon-arrow-right\"></div>");
-
-                var arrow = workSample.find(".icon-arrow-right");
-
-                console.log("Arrow APPEND");
-
-                arrow.animate({
-	                "margin-left": "5%"
-	            }, 450).animate({
-	                "margin-left": ""
-	            }, 450);
-
-			}
-
-
-
-
-//            if (! workSample.hasClass("arrow")) {
-//
-//            	workSample.addClass("arrow");
-//
-//                console.log("arrow");
-//
-//                var interval = setInterval(function () {
-//
-//                    console.log("END");
-//
-//                    clearInterval(interval);
-//
-// //                   workSample.removeClass("arrow");
-//
-//                }, 900);
-//
-//			}
-
-
-		}
-
-	}).mouseleave(function() {
-
-//        console.log("out");
-//        $(this).attr("data-mouseover", "false");
-
-    });
-/**/
 
 /**/
-	$(".work-sample a").mouseover(function() {
+	$(".work-sample a, .about a").mouseover(function() {
 
 //		console.log("over");
 
-        var workSample = $(this);
+        var a = $(this);
 
-        workSample.attr("data-mouseover", "true");
+        a.attr("data-mouseover", "true");
 
-		if (! workSample.hasClass("arrow")) {
+		if (! a.hasClass("arrow")) {
 
  //           console.log("over arrow animated");
 
-            workSample.addClass("arrow animated");
+            a.addClass("arrow animated");
 
             var interval = setInterval(function () {
                 clearInterval(interval);
 
  //               console.log("over animated END");
 
-                workSample.removeClass("animated");
+                a.removeClass("animated");
 
-                if (workSample.attr("data-mouseover") === "false") {
+                if (a.attr("data-mouseover") === "false") {
 
  //                   console.log("over arrow END");
 
-                    workSample.removeClass("arrow");
+                    a.removeClass("arrow");
 				}
 
 			}, 900);
